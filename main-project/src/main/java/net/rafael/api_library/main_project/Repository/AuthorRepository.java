f97db1853c05ed30ffe643f7e3a31614aa9b5334
rafael-men <rafaelgoncalves1822@gmail.com>
2024-11-08 18:53:57 -0300

    author-controller-done

--- main-project/src/main/java/net/rafael/api_library/main_project/Repository/AuthorRepository.java
+++ main-project/src/main/java/net/rafael/api_library/main_project/Repository/AuthorRepository.java
@@ -4,8 +4,13 @@ import net.rafael.api_library.main_project.Models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
+import java.util.List;
 import java.util.UUID;
 
 
 public interface AuthorRepository extends JpaRepository<Author, UUID> {
+
+    List<Author> findByName(String name);
+    List<Author> findByFrom(String from);
+    List<Author> findByNameAndFrom(String name,String from);
 }
--- main-project/src/main/java/net/rafael/api_library/main_project/Services/AuthorService.java
+++ main-project/src/main/java/net/rafael/api_library/main_project/Services/AuthorService.java
@@ -4,6 +4,10 @@ import net.rafael.api_library.main_project.Models.Author;
 import net.rafael.api_library.main_project.Repository.AuthorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class AuthorService {
 
@@ -15,5 +19,33 @@ public class AuthorService {
 
     public Author save(Author author) {
         return authorRepository.save(author);
+    }
+
+    public Optional<Author> findById(UUID id) {
+        return authorRepository.findById(id);
+    }
+
+    public void deleteAuthor(Author author) {
+        authorRepository.delete(author);
+    }
+
+    public List<Author> searchWithFilters(String name,String from) {
+        if(name != null && from != null) {
+            return authorRepository.findByNameAndFrom(name,from);
+        }
+        if (name != null) {
+            return authorRepository.findByName(name);
+        }
+        if (from != null) {
+            return authorRepository.findByFrom(from);
+        }
+        return authorRepository.findAll();
+    }
+
+    public void updateAuthor(Author author) {
+        if(author.getId() == null) {
+            throw new IllegalArgumentException("Author must be saved in the database");
+        }
+        authorRepository.save(author);
     }
 }
