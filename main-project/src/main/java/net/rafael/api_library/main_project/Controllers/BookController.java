dc334c8bf27a8b5f8dbc33d22d97d296784db312
rafael-men <rafaelgoncalves1822@gmail.com>
2024-11-18 21:16:09 -0300

    post and delete book methods

--- main-project/src/main/java/net/rafael/api_library/main_project/Controllers/BookController.java
+++ main-project/src/main/java/net/rafael/api_library/main_project/Controllers/BookController.java
@@ -2,32 +2,68 @@ package net.rafael.api_library.main_project.Controllers;
 
 import jakarta.validation.Valid;
 import net.rafael.api_library.main_project.Dto.BookDto;
+import net.rafael.api_library.main_project.Dto.SearchBookDto;
 import net.rafael.api_library.main_project.Exceptions.DuplicatedRegisterException;
-import net.rafael.api_library.main_project.Repository.BookRepository;
-import net.rafael.api_library.main_project.Repository.AuthorRepository;
+import net.rafael.api_library.main_project.Mapper.BookMapper;
+import net.rafael.api_library.main_project.Models.Book;
 import net.rafael.api_library.main_project.Services.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/books")
-public class BookController {
+public class BookController implements GenericController{
 
     private final BookService service;
+    private final BookMapper bookMapper;
 
 
-    public BookController(BookService service) {
+    public BookController(BookService service, BookMapper bookMapper) {
         this.service = service;
+        this.bookMapper = bookMapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SearchBookDto>> getAllBooks() {
+        List<Book> books = service.findAllBooks();
+        List<SearchBookDto> bookDtos = books.stream()
+                .map(bookMapper::toDTO)
+                .toList();
+        return ResponseEntity.ok(bookDtos);
     }
 
     @PostMapping("/new")
     public ResponseEntity<Object> createBook(@RequestBody @Valid BookDto dto) {
         try {
-            return ResponseEntity.ok(dto);
+            Book book = bookMapper.toEntity(dto);
+            service.saveBook(book);
+            var url = generateHeaderLocation(book.getId());
+            return ResponseEntity.created(url).build();
         } catch (DuplicatedRegisterException e) {
-            return (ResponseEntity<Object>) ResponseEntity.status(HttpStatus.BAD_REQUEST);        }
+            return (ResponseEntity<Object>) ResponseEntity.status(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SearchBookDto> getBooksbyId(@PathVariable("id") String id) {
+        return service.findById(UUID.fromString(id))
+                .map(book -> {
+                    var dto = bookMapper.toDTO(book);
+                    return ResponseEntity.ok(dto);
+                }).orElseGet(()-> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Object> delete(@PathVariable("id") String id) {
+        return service.findById(UUID.fromString(id))
+                .map(book -> {
+                    service.deleteBook(book);
+                    return ResponseEntity.noContent().build();
+                }).orElseGet(()-> ResponseEntity.notFound().build());
     }
 }
---
+++ main-project/src/main/java/net/rafael/api_library/main_project/Controllers/GenericController.java
@@ -0,0 +1,17 @@
+package net.rafael.api_library.main_project.Controllers;
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.UUID;
+
+public interface GenericController {
+
+    default URI generateHeaderLocation(UUID id) {
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
+    }
+}
