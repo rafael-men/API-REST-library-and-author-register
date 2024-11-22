7ea478c01c60134f20def622463068c3bc43fa37
rafael-men <rafaelgoncalves1822@gmail.com>
2024-11-21 21:39:42 -0300

    updates

--- main-project/src/main/java/net/rafael/api_library/main_project/Repository/BookRepository.java
+++ main-project/src/main/java/net/rafael/api_library/main_project/Repository/BookRepository.java
@@ -1,10 +1,14 @@
 package net.rafael.api_library.main_project.Repository;
 
-
 import net.rafael.api_library.main_project.Models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.UUID;
 
-
 public interface BookRepository extends JpaRepository<Book, UUID> {
+
+    @Query("SELECT b FROM Book b JOIN FETCH b.author WHERE b.id = :bookId")
+    Book findBookWithAuthor(@Param("bookId") UUID bookId);
 }
