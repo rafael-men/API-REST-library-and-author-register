547a26422d2c1ec980956951aec027d7cce6af2e
rafael-men <rafaelgoncalves1822@gmail.com>
2025-02-24 18:48:00 -0300

    jwk and jwt auth support

--- main-project/src/main/java/net/rafael/api_library/main_project/Config/AuthServerConfig.java
+++ main-project/src/main/java/net/rafael/api_library/main_project/Config/AuthServerConfig.java
@@ -2,16 +2,40 @@ package net.rafael.api_library.main_project.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.time.Duration;
 
 @Configuration
 @EnableWebSecurity
 public class AuthServerConfig {
+
+
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
+        httpSecurity.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());
+        httpSecurity.oauth2ResourceServer(oauth2Rs -> oauth2Rs.jwt(Customizer.withDefaults()));
+        httpSecurity.formLogin(configurer -> configurer.loginPage("/login"));
+        return httpSecurity.build();
+    }
 
     @Bean
     public TokenSettings tokenSettings() {
--- main-project/src/main/java/net/rafael/api_library/main_project/Config/SecurityConfiguration.java
+++ main-project/src/main/java/net/rafael/api_library/main_project/Config/SecurityConfiguration.java
@@ -1,5 +1,10 @@
 package net.rafael.api_library.main_project.Config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import net.rafael.api_library.main_project.Security.CustomUserDetails;
 import net.rafael.api_library.main_project.Security.LoginSocialSuccessHandler;
 import net.rafael.api_library.main_project.Services.UserService;
@@ -11,10 +16,21 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
@@ -37,12 +53,31 @@ public class SecurityConfiguration {
                     oauth2
                             .loginPage("/login").successHandler(successHandler);
                 })
+                .oauth2ResourceServer(oauth2Rs-> oauth2Rs.jwt(Customizer.withDefaults()))
                 .build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults("");
+    }
+
+    // configura o scope no jwt
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var authorititesConverter = new JwtGrantedAuthoritiesConverter();
+        authorititesConverter.setAuthorityPrefix("");
+
+        var converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(authorititesConverter);
+
+        return converter;
     }
 
     // Usuários em Memória
@@ -62,5 +97,31 @@ public class SecurityConfiguration {
         //return new InMemoryUserDetailsManager(user1,user2);
 
         return new CustomUserDetails(service);
+    }
+
+    // Configurar o JWK para gerar uma chave RSA
+    public JWKSource<SecurityContext> jwkSource() throws Exception {
+        RSAKey rsaKey = generateRSAKey();
+        JWKSet jwkSet = new JWKSet();
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    // Gerar chaves RSA
+    private RSAKey generateRSAKey() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        return new RSAKey.Builder(publicKey).privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+
+    @Bean
+    public JwtDecoder decoder (JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
     }
 }
